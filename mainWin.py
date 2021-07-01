1026bd7ff4c79d8841bed7326b9b43cc2bcdbf36
ARIFFIZZUDIN98 <s201502449@studentmail.unimap.edu.my>
2021-07-01 16:10:35 +0800

    Update mainWin.py
    to add window title

--- mainWin.py
+++ mainWin.py
@@ -12,3 +12,24 @@ Img_butt=Button(window, text="Select Image Files", width=30)
 Img_butt.grid(row =3, column = 0, padx=120, pady =20)
 
 window.mainloop()
+
+#to add window title 
+from tkinter import *
+
+def openWordfile():
+    pass
+
+def openImagefile():
+    pass
+
+window = Tk()
+window.title("Words & Images to PDF Converter (Student Edition)")
+window.geometry('450x200')
+
+Word_butt=Button(window, text="Select Word Files", width=30, command=openWordfile)
+Word_butt.grid(row =2, column = 0, padx=120, pady =20) 
+
+Img_butt=Button(window, text="Select Image Files", width=30, command=openImagefile)
+Img_butt.grid(row =3, column = 0, padx=120, pady =20)    
+
+window.mainloop()
