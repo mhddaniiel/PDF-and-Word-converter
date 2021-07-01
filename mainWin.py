f2629332f2114ee7c0d66a5322be34ce3d8a3311
ARIFFIZZUDIN98 <s201502449@studentmail.unimap.edu.my>
2021-07-01 17:09:46 +0800

    editing add window title
    to remove window mainloop

--- mainWin.py
+++ mainWin.py
@@ -11,4 +11,23 @@ Word_butt.grid(row =2, column = 0, padx=120, pady =20)
 Img_butt=Button(window, text="Select Image Files", width=30)
 Img_butt.grid(row =3, column = 0, padx=120, pady =20)
 
+#to add window title 
+from tkinter import *
+
+def openWordfile():
+    pass
+
+def openImagefile():
+    pass
+
+window = Tk()
+window.title("Words & Images to PDF Converter (Student Edition)")
+window.geometry('450x200')
+
+Word_butt=Button(window, text="Select Word Files", width=30, command=openWordfile)
+Word_butt.grid(row =2, column = 0, padx=120, pady =20) 
+
+Img_butt=Button(window, text="Select Image Files", width=30, command=openImagefile)
+Img_butt.grid(row =3, column = 0, padx=120, pady =20)    
+
 window.mainloop()
