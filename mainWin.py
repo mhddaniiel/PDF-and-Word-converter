fd108d2f34bd47ff66d4907b03a33ecb2c77da3c
wakkendul <86454422+wakkendul@users.noreply.github.com>
2021-07-02 10:48:07 +0800

    ArrageProg1

--- mainWin.py
+++ mainWin.py
@@ -1,18 +1,4 @@
 from tkinter import *
-#To setup GUI canvas using python code.
-window = Tk()
-window.title("Words & Images to PDF Converter (Student Edition)")
-window.geometry('450x200')
-
-#To create Button at GUI
-Word_butt=Button(window, text="Select Word Files", width=30)
-Word_butt.grid(row =2, column = 0, padx=120, pady =20) 
-
-Img_butt=Button(window, text="Select Image Files", width=30)
-Img_butt.grid(row =3, column = 0, padx=120, pady =20)
-
-#to add window title 
-from tkinter import *
 
 def openWordfile():
     pass
