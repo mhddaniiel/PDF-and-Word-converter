9d4e09bf690da4d4c32d1d7ea1ae81a574e436da
wakkendul <86454422+wakkendul@users.noreply.github.com>
2021-07-06 20:05:10 +0800

    Word n Img Convert Part Complete

--- mainWin.py
+++ mainWin.py
@@ -1,40 +1,6 @@
 from tkinter import *
 from PIL import Image
-from docx2pdf import convert
-from tkinter import filedialog
-
-def openWordfile():
-    file = filedialog.askopenfile(filetypes=[('word', '*.docx')])
-    convert(file.name)
-
-files = {}
-def openImagefile():
-    global files
-    files['filename']=filedialog.askopenfilenames(filetypes=[('JPG','*.jpg'),('PNG','*.png'),('JPEG','*.jpeg')])
-
-    img_list = []
-
-    for file in files['filename']:
-        img_list.append(Image.open(file).convert('RGB'))
-        img_list.append
-        
-    save_file_name = filedialog.asksaveasfilename(filetypes = [('PDF','*.pdf')])
-    img_list[0].save(f'{save_file_name}.pdf', save_all = True, append_images = img_list[1:])
-
-window = Tk()
-window.title("Word & Image convert to PDF (Student Edition)")
-window.geometry('450x200')
-
-Word_butt=Button(window, text="Select Word Files", width=30, command=openWordfile)
-Word_butt.grid(row =2, column = 0, padx=120, pady =20) 
-
-Img_butt=Button(window, text="Select Image Files", width=30, command=openImagefile)
-Img_butt.grid(row =3, column = 0, padx=120, pady =20)    
-
-#To Create MessageBox
-
-from tkinter import *
-from PIL import Image
+from pygame import mixer
 from docx2pdf import convert
 from tkinter import filedialog
 from tkinter import messagebox
@@ -63,12 +29,29 @@ def openImagefile():
 
 window=Tk()
 window.title("Words & Images to PDF Converter (Student Edition)")
-window.geometry('450x200')
+window.geometry('500x500+520+100')
+window.iconbitmap(r'icon.ico')
+window.resizable(0,0)
 
-Word_butt=Button(window, text="Select Word Files", width=30, command=openWordfile)
-Word_butt.grid(row =2, column = 0, padx=120, pady =20) 
+mixer.init()
 
-Img_butt=Button(window, text="Select Image Files", width=30, command=openImagefile)
-Img_butt.grid(row =3, column = 0, padx=120, pady =20)   
+mixer.music.load('TheFatRat - Unity.mp3')
+mixer.music.play(loops=3)
+
+back_img = PhotoImage(file='canvas.png')
+img_back = Label(window, image=back_img)
+img_back.place(relwidth=1, relheight=1)
+
+butt_word = PhotoImage(file='wordButton.png')
+Word_butt=Button(window, image=butt_word, width=100, height=50, borderwidth=0, command=openWordfile)
+Word_butt.place(relx=0.48,rely=0.86) 
+
+butt_img = PhotoImage(file='imageButton.png')
+Img_butt=Button(window, image=butt_img, width=100, height=50, borderwidth=0, command=openImagefile)
+Img_butt.place(relx=0.7,rely=0.86)   
+
+butt_exit = PhotoImage(file='exitButton.png')
+Exit_butt=Button(window, image=butt_exit, width=100, height=50, borderwidth=0, command=window.destroy)
+Exit_butt.place(relx=0.1,rely=0.86)   
 
 window.mainloop()
